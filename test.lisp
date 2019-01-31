10b1b5569b166ab6618dcb7810814d62313af0a7
Timur Ismagilov <bouncepaw2@ya.ru>
2019-01-31 17:05:37 +0500

    Edit test.lisp to also test comments

--- test.lisp
+++ test.lisp
@@ -1,4 +1,4 @@
-(start composing "your life" {freely "as you may wish {}()[]"}
+(start composing "your life" {freely "as you may wish {}()[]"} ; a comment
   [but be careful! life is extremely dangerous : "out there"])
-
+; a comment
 {behold!}
