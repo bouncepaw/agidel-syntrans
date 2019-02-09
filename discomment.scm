9607fee7be11d17d0fbdb1092ada79e8444b05a8
Timur Ismagilov <bouncepaw2@ya.ru>
2019-02-09 22:51:46 +0500

    Make `discomment.scm` use `add-to-list!`

--- discomment.scm
+++ discomment.scm
@@ -2,10 +2,8 @@
  agidel-syntrans.discomment
  *
  (import scheme
-         format)
-
- (define (add-to-list lst elt)
-   (append lst (list elt)))
+         (chicken base)
+         (only (agidel core) add-to-list add-to-list!))
  
  (define (main source-string) ; -> string?
    (let [[in-string? #f]
@@ -17,18 +15,22 @@
         (cond
          {in-string?
           (begin
-            (set! acc (add-to-list acc ch))
+            (add-to-list! acc ch)
             (cond
              [escaping-char? (set! escaping-char? #f)]
              [(eq? ch #\\) (set! escaping-char? #t)]
              [(eq? ch #\") (set! in-string? #f)]))}
          {(not deleting-comment?)
           (cond
-           [(eq? ch #\;) (set! deleting-comment? #t)]
-           [(eq? ch #\") (set! acc (add-to-list acc ch)) (set! in-string? #t)]
-           [else (set! acc (add-to-list acc ch))])}
+           [(eq? ch #\;)
+            (set! deleting-comment? #t)]
+           [(eq? ch #\")
+            (add-to-list! acc ch)
+            (set! in-string? #t)]
+           [else
+            (add-to-list! acc ch)])}
          {(eq? ch #\newline)
-          (set! acc (add-to-list acc ch))
+          (add-to-list! acc ch)
           (set! deleting-comment? #f)}))
       (string->list source-string))
      (list->string acc))))
