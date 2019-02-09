c45b34de04c8dfc3d660d0565dd1fa531c6e20da
Timur Ismagilov <bouncepaw2@ya.ru>
2019-02-09 22:55:59 +0500

    Make `quotify.scm` use `add-to-list!`

--- quotify.scm
+++ quotify.scm
@@ -2,7 +2,8 @@
  agidel-syntrans.quotify
  *
  (import scheme
-         (chicken base))
+         (chicken base)
+         (only (agidel core) add-to-list add-to-list!))
 
  (define (add-to-list lst elt)
    (append lst (list elt)))
@@ -18,50 +19,50 @@
            (when (eq? ch #\()
              (set! nest-level (+ nest-level 1))
              (set! mode 'list))
-           (set! acc (add-to-list acc ch))]
+           (add-to-list! acc ch)]
           [(list)
            (case ch
              [(#\")
-              (set! acc (add-to-list acc (string->list "\"\\\"")))
+              (add-to-list! acc (string->list "\"\\\""))
               (set! mode 'string)]
              [(#\()
-              (set! acc (add-to-list acc ch))
+              (add-to-list! acc ch)
               (set! nest-level (+ nest-level 1))]
              [(#\))
-              (set! acc (add-to-list acc ch))
+              (add-to-list! acc ch)
               (set! nest-level (- nest-level 1))]
              [(#\space #\newline #\tab)
-              (set! acc (add-to-list acc ch))]
+              (add-to-list! acc ch)]
              [else
-              (set! acc (add-to-list acc (list #\" ch)))
+              (add-to-list! acc (list #\" ch))
               (set! mode 'symbol)])]
           [(symbol)
            (case ch
              [(#\()
               (set! mode 'list)
-              (set! acc (add-to-list acc (string->list "\" (")))
+              (add-to-list! acc (string->list "\" ("))
               (set! nest-level (+ nest-level 1))]
              [(#\))
               (set! nest-level (- nest-level 1))
-              (set! acc (add-to-list acc (string->list "\")")))
+              (add-to-list! acc (string->list "\")"))
               (set! mode 'list)]
              [(#\space #\newline #\tab)
               (set! mode 'list)
-              (set! acc (add-to-list acc (list #\" ch)))]
+              (add-to-list! acc (list #\" ch))]
              [else
-              (set! acc (add-to-list acc ch))])]
+              (add-to-list! acc ch)])]
           [(string)
            (case ch
              [(#\\)
-              (set! acc (add-to-list acc ch))
+              (add-to-list! acc ch)
               (set! mode 'escaping-char)]
              [(#\")
-              (set! acc (add-to-list acc (string->list "\\\"\"")))
+              (add-to-list! acc (string->list "\\\"\""))
               (set! mode 'list)]
              [else
-              (set! acc (add-to-list acc ch))])]
+              (add-to-list! acc ch)])]
           [(escaping-char)
-           (set! acc (add-to-list acc ch))
+           (add-to-list! acc ch)
            (set! mode 'string)]))
       (string->list source-string))
      (list->string (flatten acc)))))
