92c6d723072db75b6a8c3117056d13c5bf65179c
Timur Ismagilov <bouncepaw2@ya.ru>
2019-02-09 22:48:10 +0500

    Make `disbrace.scm` use `add-to-list!`

--- disbrace.scm
+++ disbrace.scm
@@ -2,36 +2,33 @@
  agidel-syntrans.disbrace
  *
  (import scheme
-         (chicken base))
+         (chicken base)
+         (only (agidel core) add-to-list add-to-list!))
 
- (define (add-to-list lst elt)
-   (append lst (list elt)))
- 
  (define (main source-string)
    (let [[in-string? #f]
          [escaping-char? #f]
          [pairs-in-search 0]
          [wrapping-brace-expr? #f]
-         
          [acc '()]]
      (for-each
       (lambda (ch)
         (if in-string?
             (cond
              [escaping-char?
-              (set! acc (add-to-list acc ch))
+              (add-to-list! acc ch)
               (set! escaping-char? #f)]
              [in-string?
               (cond
                [(eq? ch #\")
                 (set! in-string? #f)
-                (when wrapping-brace-expr? (set! acc (add-to-list acc #\\)))]
+                (when wrapping-brace-expr? (add-to-list! acc #\\))]
                [(eq? ch #\\) (set! escaping-char? #t)])
-              (set! acc (add-to-list acc ch))])
+              (add-to-list! acc ch)])
             (cond
              [(eq? ch #\{)
               (if wrapping-brace-expr?
-                  (set! acc (add-to-list acc #\{))
+                  (add-to-list! acc #\{)
                   (begin (set! acc (add-to-list acc (string->list
                                                      "(_brace \"")))
                          (set! wrapping-brace-expr? #t)))
@@ -39,18 +36,15 @@
              [(eq? ch #\})
               (set! pairs-in-search (- pairs-in-search 1))
               (if (zero? pairs-in-search)
-                  (begin (set! acc (add-to-list acc (string->list "\")")))
+                  (begin (add-to-list! acc (string->list "\")"))
                          (set! wrapping-brace-expr? #f))
-                  (set! acc (add-to-list acc #\})))
-              ]
+                  (add-to-list! acc #\}))]
              [(eq? ch #\")
-              (if wrapping-brace-expr?
-                  (begin (set! acc (add-to-list acc (string->list "\\\"")))
-                         (set! in-string? #t))
-                  (begin (set! in-string? #t)
-                         (set! acc (add-to-list acc ch))))]
+              (add-to-list! acc
+                            (if wrapping-brace-expr? (string->list "\\\"") ch))
+              (set! in-string? #t)]
              [else
-              (set! acc (add-to-list acc ch))])))
+              (add-to-list! acc ch)])))
       (string->list source-string))
      (when (not (zero? pairs-in-search))
        (display "Agidel: unmatched brace pair. Exiting.")
