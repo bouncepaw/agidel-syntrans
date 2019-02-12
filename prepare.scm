c370df6d73d6ad31e7aaacdcdb080c32ae43d0fa
Timur Ismagilov <bouncepaw2@ya.ru>
2019-02-12 19:19:17 +0500

    Add prepare syntrans placeholder

---
+++ prepare.scm
@@ -0,0 +1,11 @@
+(module
+ agidel-syntrans.prepare
+ (main)
+ (import scheme
+         (chicken base)
+         (agidel core)
+         (agidel plugin)
+         (srfi 69))
+
+ (define (main source-string)
+   source-string))
