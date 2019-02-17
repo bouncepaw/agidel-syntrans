1a055e58f8237690331300baa20cdcc4b12d1ba2
Timur Ismagilov <bouncepaw2@ya.ru>
2019-02-17 22:00:57 +0500

    Add `main` to `run` syntrans

--- run.scm
+++ run.scm
@@ -10,6 +10,17 @@
          (srfi 69)
          (clojurian syntax)
          format)
- 
- (define (main source-tree plugin-list)
-   source-tree))
+
+ ;; `source-tree` is a LIST, not a STRING!
+ ;; This functions outputs a string though.
+ (define (main source-tree plugins)
+   ;; Import all plugins, prefixing Agidel macros from them with '/agidel/.
+   (->> plugins
+        (map (lambda (p)
+               `(prefix ,(symbol-append 'agidel-plugin. p) /agidel/)))
+        (cons 'import)
+        eval)
+   ;; Eval it, convert to string.
+   (-> source-tree
+       eval
+       (string-join "\n" 'infix)))
